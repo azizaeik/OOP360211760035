dbfa75371ecc6d6da3c7ee3ee145c490f27c17d9
aziza <nittay502@gmail.com>
2018-05-08 16:13:56 +0700

    Initial commit

---
+++ src/OPP360211760035/Hello.java
@@ -0,0 +1,13 @@
+package OPP360211760035;
+public class Hello {
+    public static  void main (String[]args) {
+
+        System.out.printlin("Hello,My name is aziza");
+        System.out.printlin("Major:Information System");
+        System.out.println("Faculty:Managment Technology");
+        System.out.printlin("Major:Information System");
+
+    } //main
+
+}
+}
