8ae9c23fae64bb0fc4080cb67eff64f1a06006cd
LabCom_4 <nittya502>
2018-05-09 12:38:28 +0700

    student

---
+++ src/lad5/lab5/arryOperator.java
@@ -0,0 +1,56 @@
+package lab5;
+
+        import java.util.Scanner;
+
+//1. method for input data  (10)
+//2. method for show data in array
+//3. method to find average value in array
+//4. method to find maximum value in array
+//5. method to find minimum value in array
+public class arrayOperator {
+    public static final int MAX = 10;
+
+    public static void main(String[] args) {
+        int myArray[] = new int[MAX];
+        myArray = inputData(myArray);
+        showData(myArray);
+//find average
+        findAverage(myArray);
+
+
+
+    }  //main
+
+    private static void findAverage(int[] myArray) {
+        int max = myArray [0];
+        for (int i = 0; i <myArray.length;i++){
+            if ( (max<myArray[i])
+                max = myArray[i];
+
+        System.out.println("The average value is"+
+                al/MAX));
+
+
+    }
+
+}//main
+            private static void showData(int[] myArray) {
+                System.out.println("Data in array: ");
+                for (int val:myArray)
+                        System.out.print(val+"\t");
+           }
+           private static int[] inputData(int[] myArray) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Please enter integers: ");
+                for (int i = 0; i < myArray.length; i++) {
+                        System.out.print("integer "+i+": ");
+                        myArray[i] = sc.nextInt();
+                   }
+                return myArray;
+
+
+                           }
+
+
+        }
+}//class
--- src/lad5/myArray.java
+++ src/lad5/myArray.java
@@ -1,5 +1,7 @@
 package lab5;
 
+import com.sun.java_cup.internal.runtime.Scanner;
+
 public class myAerray {
     public static void main(String[] args) {
 
@@ -11,13 +13,14 @@ public class myAerray {
         Scanner sc = new Scanner(System.in);
         for (int i=0;i<a.length; i++){
             System.out.print("a["+i+"]: ");
-            a[i] = sc.nexlnt();
+            switch (a[i] = sc.nexlnt()) {
+            }
         }
 
         System.out.println("data in array : ");
         for (int i=0;i<a.length;i++){
             System.out.print (a[i]+"\t");
-
+    private static  int []inputData (int)[]
         }
 
     }
