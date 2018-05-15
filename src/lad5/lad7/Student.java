2488f5cd2ae15e5a90b131649afb9102c13da6f6
Aziza <nittay502@gmail.com>
2018-05-15 14:33:48 +0700

    student

--- src/lad5/lad7/Student.java
+++ src/lad5/lad7/Student.java
@@ -28,8 +28,7 @@ public  class Student {
                 '}';
     }
 
-    public String getName() {
-        return name;
+    public String getName() { return name;
     }
 
     public void setName(String name) {
